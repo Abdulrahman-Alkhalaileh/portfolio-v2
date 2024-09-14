383bb50cd2c2969115201b7bd88aade3d51fdd44
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-14 23:34:39 +0300

    added the new hook to handle header scrolling animation

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,29 +1,34 @@
-"use client";
 import React from "react";
 import { Stack, StackProps } from "@mui/material";
 import SectionsList from "components/Lists/SectionsList";
 import ThemeMenu from "./ThemeMenu";
 import SectionsMenu from "./SectionsMenu";
 import Logo from "./Logo";
+import { motion } from "framer-motion";
+import useScrollDirection from "customHooks/useScrollDirection";
 
-export interface HeaderProps extends StackProps{}
+export interface HeaderProps extends StackProps {}
 
 const Header: React.FC<HeaderProps> = ({ ...props }) => {
+  const scrollDirection = useScrollDirection("up");
+
   return (
     <Stack
-      direction="row"
+      component={motion.div}
+      direction='row'
       alignItems="center"
       justifyContent="space-between"
-      spacing={1}
+      gap={1}
       px={{ xs: 2, md: 12 }}
       position="fixed"
       zIndex={1000}
       boxSizing="border-box"
       width="100%"
-      height={{xs:80,sm:100}}
+      height={{ xs: 80, sm: 100 }}
       bgcolor="background.paper"
       borderBottom={2}
-      borderColor='background.default'
+      borderColor="background.default"
+      animate={{ top: scrollDirection === "down" ? -100 : 0 }}
       {...props}
     >
       <SectionsMenu
