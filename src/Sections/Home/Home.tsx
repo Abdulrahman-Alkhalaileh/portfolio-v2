03729958442aa073f22c452dd3eb58fc59357768
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-14 04:33:44 +0300

    enhance themes and update theme menu coloring

--- src/Sections/Home/Home.tsx
+++ src/Sections/Home/Home.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import H1 from 'components/Typography/H1';
-import DraggableElement from 'components/Animations/DraggableIndecator';
+import React from "react";
+import H1 from "components/Typography/H1";
+import PageTransition from "components/Animations/PageTransition";
+import { Box, CircularProgress } from "@mui/material";
 
-export interface HomeProps {
-  
-}
+export interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({ ...props }) => {
   return (
-    <>
-      <H1>Abdulrahman Alkhalaileh</H1>
-      <H1>Still In Progress...</H1>
-      <DraggableElement direction='vertical' />
-    </>
-  )
-}
+    <PageTransition>
+      <H1 textAlign="center">Abdulrahman Alkhalaileh</H1>
+      <H1 textAlign="center" gutterBottom>Still In Progress...</H1>
+      <Box textAlign="center">
+        <CircularProgress size={70} />
+      </Box>
+    </PageTransition>
+  );
+};
 
 export default Home;
