6ed3c8b50f976225e6eb127ff678c114b7ba8b89
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-15 04:47:19 +0300

    added icons for secitons list

--- src/configs/sectionItems.tsx
+++ src/configs/sectionItems.tsx
@@ -1,13 +1,21 @@
-import Experience from "Sections/Experience/Experience";
 import Home from "Sections/Home/Home";
-import Projects from "Sections/Projects/Projects";
+import HomeIcon from "@mui/icons-material/Home";
+
 import Skills from "Sections/Skills/Skills";
+import ContactEmergencyIcon from "@mui/icons-material/ContactEmergency";
+
+import Experience from "Sections/Experience/Experience";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+
+import Projects from "Sections/Projects/Projects";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
 
 export interface AnimatedItemType {
   index: number;
   name: string;
-  element: React.ReactNode;
+  element?: React.ReactNode;
   path?: string;
+  icon?: React.ReactNode;
 }
 
 export const sections: AnimatedItemType[] = [
@@ -16,18 +24,27 @@ export const sections: AnimatedItemType[] = [
     name: "home",
     element: <Home />,
     path: "/",
+    icon: <HomeIcon />,
   },
   {
     index: 1,
     name: "skills",
     element: <Skills />,
     path: "/skills",
+    icon: <ContactEmergencyIcon />,
   },
   {
     index: 2,
     name: "experience",
     element: <Experience />,
     path: "/experience",
+    icon: <BusinessCenterIcon />,
   },
-  { index: 3, name: "projects", element: <Projects />, path: "/projects" },
+  {
+    index: 3,
+    name: "projects",
+    element: <Projects />,
+    path: "/projects",
+    icon: <AccountTreeIcon />,
+  },
 ];
