5c954658f4da7d2527f24961dd9a4011086c1452
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-17 23:57:01 +0300

    added hero section to home page

--- src/Sections/Home/Home.tsx
+++ src/Sections/Home/Home.tsx
@@ -1,63 +1,30 @@
 import React, { useEffect, useState } from "react";
-import H1 from "components/Typography/H1";
 import PageTransition from "components/Animations/PageTransition";
 import { Stack } from "@mui/material";
 import P1 from "components/Typography/P1";
-import { HighlightPhrases } from "utils/highlightText";
 import { fetchData } from "helpers/fetchData";
-import { SkillsType } from "helpers/types";
+import { ProjectType, SkillsType } from "helpers/types";
 import H2 from "components/Typography/H2";
 import Slider from "components/Slider/AutoScrollSlider";
-import AnimatedPic from "components/Animations/AnimatedPic";
+import HeroSection from "./partials.tsx/HeroSection";
 
 export interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({ ...props }) => {
   const [skills, setSkills] = useState<SkillsType[]>([]);
+  const [projects, setProjects] = useState<ProjectType[]>([]);
   useEffect(() => {
     fetchData("technicalSkills").then((data) =>
       setSkills(data as SkillsType[])
     );
+    fetchData("projects").then((data) => setProjects(data as ProjectType[]));
   }, []);
-
-  const summary =
-    HighlightPhrases(`A reliable Frontend engineer with with experience in ReactJS,
-            NextJS, Material UI, and API integration. Skilled in building
-            responsive, dynamic web applications and collaborating with backend
-            teams. A fast autonomous learner with a strong focus on delivering
-            clean, efficient code and enhancing user experience.`);
+  console.log(projects)
 
   return (
     <PageTransition>
       <Stack spacing={5}>
-        <Stack
-          spacing={{xs:0,md:6}}
-          direction={{ xs: "column", md: "row" }}
-          bgcolor="background.default"
-          alignItems="center"
-          borderRadius={5}
-          p={3}
-        >
-          <Stack display={{xs:'none',md:'flex'}} pb={4}>
-              <AnimatedPic imageUrl="https://i.imgur.com/pN9xNoo.png" />
-            </Stack>
-          <Stack alignItems={{ xs: "center", lg: "start" }}>
-            <P1 fontWeight={900} textAlign="center">
-              Hi There, I'm
-            </P1>
-            <H1 gutterBottom textAlign="center">
-              Abdulrahman Alkhalaileh
-            </H1>
-            <Stack display={{xs:'flex',md:'none'}} pb={4}>
-              <AnimatedPic imageUrl="https://i.imgur.com/pN9xNoo.png" />
-            </Stack>
-            <P1
-              fontWeight={900}
-              maxWidth={1000}
-              dangerouslySetInnerHTML={{ __html: summary }}
-            ></P1>
-          </Stack>
-        </Stack>
+        <HeroSection />
         <Stack
           direction={{ xs: "column", lg: "row" }}
           justifyContent="space-around"
@@ -66,7 +33,10 @@ const Home: React.FC<HomeProps> = ({ ...props }) => {
           borderRadius={5}
           p={2}
         >
-          <H2>Experience With</H2>
+          <Stack>
+            <P1 fontWeight={900}>I have</P1>
+            <H2>Experience With</H2>
+          </Stack>
           <Slider items={skills} />
         </Stack>
       </Stack>
