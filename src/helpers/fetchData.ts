7f1ac4b25fa361792704108bfd2796b4813e87eb
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-22 04:13:37 +0300

    modify sorting to match the new functions in slice

--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -1,15 +1,20 @@
 import { collection, query, getDocs, orderBy } from "firebase/firestore";
 import { db } from "../configs/firebase";
 
+export interface SortingType {
+  field?: string;
+  order: "asc" | "desc"
+}
+
 export interface FetchDataProps {
   collectionName: string;
-  sort?: { field?: string; order: "asc" | "desc" };
+  sort?: Partial<SortingType>;
 }
 
 export const fetchData = async ({ collectionName, sort }: FetchDataProps) => {
   let q = query(collection(db, collectionName));
 
-  if (sort)
+  if (sort?.order)
     q = query(
       collection(db, collectionName),
       orderBy(sort.field || "id", sort.order)
