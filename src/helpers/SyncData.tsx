264ad44640e8d6a1b89755474dfca97d1b247c58
abdulrahman.alkhalaileh <abdulrahman.alkhalaileh@welab.health>
2024-09-22 04:57:14 +0300

    optimize performance when fetching data

--- src/helpers/SyncData.tsx
+++ src/helpers/SyncData.tsx
@@ -6,33 +6,19 @@ import {
   fetchTechnicalSkills,
   fetchUserInfo,
 } from "reduxConfigs/slices/firestoreSlice";
-import { useAppDispatch, useAppSelector } from "reduxConfigs/store";
+import { useAppDispatch } from "reduxConfigs/store";
 
 const SyncData: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const {
-    experience,
-    personalSkills,
-    projects,
-    technicalSkills,
-    userInfo,
-  } = useAppSelector((state) => state.firestoreSlice);
+  const dispatcher = useAppDispatch();
 
   useEffect(() => {
-    // Dispatch all fetch actions if data is not already present
-    if (!experience) dispatch(fetchExperience());
-    if (!personalSkills) dispatch(fetchPersonalSkills());
-    if (!projects) dispatch(fetchProjects());
-    if (!technicalSkills) dispatch(fetchTechnicalSkills({}));
-    if (!userInfo) dispatch(fetchUserInfo());
-  }, [
-    dispatch,
-    experience,
-    personalSkills,
-    projects,
-    technicalSkills,
-    userInfo,
-  ]);
+    // Dispatch all fetch actions initially
+    dispatcher(fetchExperience());
+    dispatcher(fetchPersonalSkills());
+    dispatcher(fetchProjects());
+    dispatcher(fetchTechnicalSkills({}));
+    dispatcher(fetchUserInfo());
+  }, [dispatcher]);
 
   return <></>;
 };
